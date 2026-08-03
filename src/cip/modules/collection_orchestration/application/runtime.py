05a8b3e1a42ccb88aa182ea7137e7d8962742ec1
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-08-03 20:54:59 +0200

    style(collection): organize runtime imports

--- src/cip/modules/collection_orchestration/application/runtime.py
+++ src/cip/modules/collection_orchestration/application/runtime.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
+import logging
 from collections.abc import Callable
 from dataclasses import dataclass
 from datetime import datetime
-import logging
 from os import getpid
 from socket import gethostname
 from time import sleep
