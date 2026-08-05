3eb0f8e8dc27549df91dfa97bec14c97dc0fc098
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-08-05 16:06:29 +0200

    Lot 11: initialize procurement history domain

---
+++ src/cip/modules/procurement_history/domain/__init__.py
@@ -0,0 +1,27 @@
+from cip.modules.procurement_history.domain.models import (
+    AmountType,
+    ContractStatus,
+    DateBasis,
+    MoneyAmount,
+    PartyResolutionStatus,
+    ProcurementContractProjection,
+    ProcurementParty,
+    ProcurementPublication,
+    ProcurementPublicationKind,
+    ProcurementProcedureStatus,
+    ProcurementPartyRole,
+)
+
+__all__ = [
+    "AmountType",
+    "ContractStatus",
+    "DateBasis",
+    "MoneyAmount",
+    "PartyResolutionStatus",
+    "ProcurementContractProjection",
+    "ProcurementParty",
+    "ProcurementPartyRole",
+    "ProcurementPublication",
+    "ProcurementPublicationKind",
+    "ProcurementProcedureStatus",
+]
