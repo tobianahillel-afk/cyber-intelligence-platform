5e09022b05505e7ec1e08dec5d4b35f9ee062a61
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-08-09 02:21:17 +0200

    Lot 21: export professional reconciliation contracts

--- src/cip/modules/professional_context/domain/__init__.py
+++ src/cip/modules/professional_context/domain/__init__.py
@@ -15,6 +15,20 @@ from cip.modules.professional_context.domain.person import (
     source_person_key,
 )
 from cip.modules.professional_context.domain.privacy import ProfessionalProcessingContext
+from cip.modules.professional_context.domain.projections import (
+    ProfessionalContactProjection,
+    ProfessionalPersonProjection,
+    ProfessionalRoleProjection,
+    PublicCommunityProjection,
+    ReportingLineProjection,
+)
+from cip.modules.professional_context.domain.reconciliation import (
+    reconcile_community_context,
+    reconcile_contact_evidence,
+    reconcile_person_references,
+    reconcile_reporting_claims,
+    reconcile_role_claims,
+)
 from cip.modules.professional_context.domain.relevance import ProfessionalServiceRelevance
 from cip.modules.professional_context.domain.roles import (
     ProfessionalRoleClaim,
@@ -30,12 +44,22 @@ __all__ = [
     "OrganizationLinkStatus",
     "ProfessionalClaimType",
     "ProfessionalContactEvidence",
+    "ProfessionalContactProjection",
+    "ProfessionalPersonProjection",
     "ProfessionalPersonReference",
     "ProfessionalProcessingContext",
     "ProfessionalReviewState",
     "ProfessionalRoleClaim",
+    "ProfessionalRoleProjection",
     "ProfessionalServiceRelevance",
     "PublicCommunityContext",
+    "PublicCommunityProjection",
     "ReportingLineClaim",
+    "ReportingLineProjection",
+    "reconcile_community_context",
+    "reconcile_contact_evidence",
+    "reconcile_person_references",
+    "reconcile_reporting_claims",
+    "reconcile_role_claims",
     "source_person_key",
 ]
