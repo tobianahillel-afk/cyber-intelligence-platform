355c32488523feb522b0c7baac326416c9341462
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-08-09 15:18:41 +0200

    Add Lot 22 workspace stylesheet layout

---
+++ apps/web/src/app/conditional-integrations/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import "../../features/conditional-integrations/conditional-integrations.css";
+
+export default function ConditionalIntegrationsLayout({ children }: { children: ReactNode }) {
+  return children;
+}
