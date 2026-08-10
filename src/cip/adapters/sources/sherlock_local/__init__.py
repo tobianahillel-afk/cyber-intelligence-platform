c143879eba17afa530e1926dfcc13f4bb31c683f
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-08-10 16:50:43 +0200

    SA-05: fix Sherlock adapter package exports

--- src/cip/adapters/sources/sherlock_local/__init__.py
+++ src/cip/adapters/sources/sherlock_local/__init__.py
@@ -1,6 +1,6 @@
 from cip.adapters.sources.sherlock_local.adapter import SherlockLocalAdapter
 from cip.adapters.sources.sherlock_local.registry import SherlockTarget, load_sherlock_targets
-from cip.adapters.sourcessherlock_local.runner import SherlockExecutionConfig, SherlockFinding
+from cip.adapters.sources.sherlock_local.runner import SherlockExecutionConfig, SherlockFinding
 
 __all__ = [
     "SherlockExecutionConfig",
