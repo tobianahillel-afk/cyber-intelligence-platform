742dacb69d876b18e7f4cf396f8fcb58a94cd689
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-08-11 18:21:02 +0200

    Lot 24: export need detection primitives

---
+++ src/cip/modules/need_detection/domain/__init__.py
@@ -0,0 +1,15 @@
+from cip.modules.need_detection.domain.models import (
+    EvidenceContribution,
+    EvidencePosition,
+    NeedHorizon,
+    NeedHypothesisClass,
+    NeedUrgency,
+)
+
+__all__ = [
+    "EvidenceContribution",
+    "EvidencePosition",
+    "NeedHorizon",
+    "NeedHypothesisClass",
+    "NeedUrgency",
+]
